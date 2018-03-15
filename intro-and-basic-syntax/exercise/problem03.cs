4fefc1f369d93464651aa839da86106840c127fc
SoftUni Student <alexmarcow05@gmail.com>
2018-03-15 18:22:37 +0200

    added problem 05

---
+++ intro-and-basic-syntax/exercise/problem03.cs
@@ -0,0 +1,11 @@
+﻿public class Demo
+{
+
+    public static void main(string[] args)
+    {
+        for (int x = 1; x <= 20; ++x)
+        {
+            System.Console.WriteLine(x);
+        }
+    }
+}
